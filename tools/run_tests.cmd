82968c7029196c760ff2fa5f9e329d4793786ce1
roger <roger@homesvithome.se>
2026-04-04 15:23:52 +0200

    Updated tool

--- tools/run_tests.cmd
+++ tools/run_tests.cmd
@@ -1,10 +1,20 @@
 @echo off
 
-IF EXIST "..\alex\tests\run_tests.py" (
-  pushd ..\alex\tests
-) ELSE (
-  pushd alex\tests
-)
+REM Hämta katalogen där skriptet ligger
+set SCRIPT_DIR=%~dp0
 
+REM Ta bort avslutande backslash
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+
+REM Hämta skriptets föräldrakatalog (där alex ligger)
+for %%A in ("%SCRIPT_DIR%\..") do set PROJECT_DIR=%%~fA
+
+REM Om vi står i projektroten (alex)
+IF /I "%CD%"=="%PROJECT_DIR%" (
+    pushd tests
+) ELSE (
+    pushd "%PROJECT_DIR%\tests"
+)
+echo %CD%
 python run_tests.py
 popd
