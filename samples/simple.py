c52e608497e8fac9e8636de4e8a1db757d5c6374
roger <roger@homesvithome.se>
2026-04-06 20:32:55 +0200

    Refactored sample

--- samples/simple.py
+++ samples/simple.py
@@ -3,9 +3,18 @@ from alex import Alex
 OPERATORS = (("ADD", "+"), ("SUB", "-"))
 REGEXPS = (("NUM", '^["0123456789"]*'),)
 
-lexer = Alex(operators=OPERATORS, regexps=REGEXPS)
-lexer.scan('1 + 2 - 123')
 
-print('----(Tokens found)----------')
-for token in lexer.tokens:
-    print(token)
+def run_simple():
+    lexer = Alex(operators=OPERATORS, regexps=REGEXPS)
+    lexer.scan("1 + 2 - 123")
+    print_report(lexer)
+
+
+def print_report(lexer):
+    print("----(Tokens found)----------")
+    for token in lexer.tokens:
+        print(token)
+
+
+if __name__ == "__main__":
+    run_simple()
