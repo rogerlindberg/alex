2a9d7e921ebc47c3648155edcbbed582caebec40
roger <roger@homesvithome.se>
2026-04-06 16:54:04 +0200

    Updated tool to build from parent dir

--- tools/build_dist.cmd
+++ tools/build_dist.cmd
@@ -1,7 +1,13 @@
-@echo off
+for %%i in ("%CD%") do set CURRENT_DIR=%%~nxi
+
+IF /I "%CURRENT_DIR%"=="tools" (
+    cd ..
+)
 
 rmdir /S /Q site
 rmdir /S /Q dist
 rmdir /S /Q alex_lexer.egg-info
 
 python -m build
+
+popd
